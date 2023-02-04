e15c837ee929c1bf2a12e4c2e35e324677468f43
DimaInNature <dimainnature@yandex.ru>
2023-02-04 18:16:17 +0300

    feat: Create SignUp component.

--- VictoryRestaurant.Web.Presentation/global.cs
+++ VictoryRestaurant.Web.Presentation/global.cs
@@ -3,3 +3,4 @@ global using VictoryRestaurant.Web.Domain.Enums.Options;
 global using VictoryRestaurant.Web.Domain.Models.Bookings;
 global using VictoryRestaurant.Web.Domain.Models.Contacts;
 global using VictoryRestaurant.Web.Domain.Models.Foods;
+global using VictoryRestaurant.Web.Domain.Models.Subscribers;
