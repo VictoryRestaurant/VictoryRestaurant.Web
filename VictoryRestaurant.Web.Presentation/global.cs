379dd36fbfb18375b24e555c712aadb4eaf6cd41
DimaInNature <dimainnature@yandex.ru>
2023-02-04 18:13:07 +0300

    feat: Create FoodMenu component.

--- VictoryRestaurant.Web.Presentation/global.cs
+++ VictoryRestaurant.Web.Presentation/global.cs
@@ -2,3 +2,4 @@
 global using VictoryRestaurant.Web.Domain.Enums.Options;
 global using VictoryRestaurant.Web.Domain.Models.Bookings;
 global using VictoryRestaurant.Web.Domain.Models.Contacts;
+global using VictoryRestaurant.Web.Domain.Models.Foods;
