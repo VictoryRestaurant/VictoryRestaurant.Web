f8885ea7a5f02f773c1eca5d23d5b6cc587b328f
DimaInNature <dimainnature@yandex.ru>
2023-02-04 16:53:55 +0300

    feat: Create ContactController.

---
+++ VictoryRestaurant.Web.Presentation/Controllers/ContactController.cs
@@ -0,0 +1,9 @@
+﻿namespace VictoryRestaurant.Web.Presentation.Controllers;
+
+[Route(template: "Contact")]
+public sealed class ContactController : Controller
+{
+	[ResponseCache(CacheProfileName = "Caching")]
+	[HttpGet(template: "")]
+	public IActionResult Index() => View(viewName: "Contact");
+}
