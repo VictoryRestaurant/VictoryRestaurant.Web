56b9cfbd63e6b92cb051312a91eabdff6c1fc6bd
DimaInNature <dimainnature@yandex.ru>
2023-02-04 15:44:04 +0300

    feat: Create contracts.

--- VictoryRestaurant.Web.Domain/global.cs
+++ VictoryRestaurant.Web.Domain/global.cs
@@ -1,1 +1,3 @@
-﻿
+﻿global using VictoryRestaurant.Web.Domain.Models.Bookings;
+global using VictoryRestaurant.Web.Domain.Models.Contacts;
+global using VictoryRestaurant.Web.Domain.Models.Subscribers;
