4b0a1905484b40f718913a20dcf55b1cb2fc28a2
DimaInNature <dimainnature@yandex.ru>
2023-02-04 18:10:19 +0300

    feat: Create CreateContactMessage component.

--- VictoryRestaurant.Web.Presentation/global.cs
+++ VictoryRestaurant.Web.Presentation/global.cs
@@ -1,3 +1,4 @@
 ﻿global using Microsoft.AspNetCore.Mvc;
 global using VictoryRestaurant.Web.Domain.Enums.Options;
 global using VictoryRestaurant.Web.Domain.Models.Bookings;
+global using VictoryRestaurant.Web.Domain.Models.Contacts;
