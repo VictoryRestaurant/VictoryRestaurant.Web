f333665c652f6c5878c0fa7c119f1216dfc68671
DimaInNature <dimainnature@yandex.ru>
2023-02-04 19:17:35 +0300

    feat: Create MailSubscribersController.

---
+++ VictoryRestaurant.Web.Presentation/Controllers/MailSubscribersController.cs
@@ -0,0 +1,12 @@
+﻿namespace VictoryRestaurant.Web.Presentation.Controllers;
+
+[Route(template: "/MailSubscribers")]
+public sealed class MailSubscribersController : Controller
+{
+    [HttpPost(template: "")]
+    public async Task<IActionResult> CreateMailSubscriber(
+        [FromForm] MailSubscriberModel mailSubscriber)
+    {
+        return RedirectToAction(actionName: "Index", controllerName: "Home");
+    }
+}
