fd28a77374192630aee8cbea8b566d577c32a80d
DimaInNature <dimainnature@yandex.ru>
2023-02-04 19:16:06 +0300

    feat: Create ContactMessagesController.

---
+++ VictoryRestaurant.Web.Presentation/Controllers/ContactMessagesController.cs
@@ -0,0 +1,11 @@
+﻿namespace VictoryRestaurant.Web.Presentation.Controllers;
+
+[Route(template: "/ContactMessages")]
+public sealed class ContactMessagesController : Controller
+{
+    public async Task<IActionResult> CreateContactMessage(
+        [FromForm] ContactMessageModel contactMessage)
+    {
+        return RedirectToAction(actionName: "Index", controllerName: "Contact");
+    }
+}
