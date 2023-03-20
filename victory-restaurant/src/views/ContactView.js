26317de353859e237772cbc51734b9c05eddf857
DimaInNature <dimainnature@yandex.ru>
2023-03-20 21:49:28 +0300

    refactoring: Add changes.

---
+++ victory-restaurant/src/views/ContactView.js
@@ -0,0 +1,14 @@
+import React from "react"
+import HeaderMenuComponent from "../components/HeaderMenuComponent"
+import FooterComponent from "../components/FooterComponent"
+
+function ContactView() {
+    return (
+        <>
+        <HeaderMenuComponent />
+        <FooterComponent />
+        </>
+    )
+}
+
+export default ContactView
